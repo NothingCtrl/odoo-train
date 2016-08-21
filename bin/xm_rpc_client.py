b796b66dbb5291c86ac2121afe0b28dd280ded04
NothingCtrl <nothingctrl@gmail.com>
2016-08-22 03:01:00 +0700

    Completed Building a Module guide for Odoo 9

--- bin/xm_rpc_client.py
+++ bin/xm_rpc_client.py
@@ -23,21 +23,34 @@
 #     'name': 'My session',
 #     'course_id': 2,
 # })
-
+import functools
 import xmlrpclib
+HOST = 'localhost'
+PORT = 8069
+DB = 'OdooTrain'
+USER = 'admin'
+PASS = 'admin'
+ROOT = 'http://%s:%d/xmlrpc/2/' % (HOST, PORT)
 url = 'http://localhost:8069'
-db = 'OdooTrain'
-username = 'admin'
-password = 'admin'
 
-common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+common = xmlrpclib.ServerProxy(ROOT + 'common'.format(url))
 print common.version()
 
-uid = common.authenticate(db, username, password, {})
+uid = common.authenticate(DB, USER, PASS, {})
 
-models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
-sessions = models.execute_kw(db, uid, password, 'openacademy.session', 'search', [])
+call = functools.partial(
+    xmlrpclib.ServerProxy(ROOT + 'object').execute_kw,
+    DB, uid, PASS)
+
+sessions = call('openacademy.session', 'search_read', [], {'fields': ['name', 'seats']})
 
 for session in sessions:
     print "Session %s (%s seats)" % (session['name'], session['seats'])
 
+# 3.create a new session
+course_id = call('openacademy.course', 'search', [[['name', 'ilike', 'Course']]])[0]
+
+session_id = call('openacademy.session', 'create', [{
+    'name': 'My session 22',
+    'course_id': course_id,
+}])
